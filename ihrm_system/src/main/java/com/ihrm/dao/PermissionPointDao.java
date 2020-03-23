0a720a83f30a6f5656d7f2ff62414440fbbcab1f
huangzhenmu <huangzhenmu@ruixiude.com>
2020-03-23 23:35:43 +0800

    权限模块增删改查

--- ihrm_system/src/main/java/com/ihrm/dao/PermissionPointDao.java
+++ ihrm_system/src/main/java/com/ihrm/dao/PermissionPointDao.java
@@ -1,5 +1,9 @@
 package com.ihrm.dao;
 
-public interface PermissionPointDao {
+import com.ihrm.system.PermissionPoint;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface PermissionPointDao extends JpaRepository<PermissionPoint,String>, JpaSpecificationExecutor<PermissionPoint> {
 
 }
